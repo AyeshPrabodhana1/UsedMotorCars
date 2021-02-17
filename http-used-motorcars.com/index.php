a83935a1dd13f1a0a809dd2c6556c87d7900dd79
suren vithanage <surenanthonyvithanage@gmail.com>
2021-02-17 09:12:36 +0530

    Updated banner

--- http-used-motorcars.com/index.php
+++ http-used-motorcars.com/index.php
@@ -7,11 +7,11 @@
 		<div class="img-fill">
 			<!-- <img src="assets/images/banner/banner_flat_10.jpg" alt="banner"> -->
 			<!-- <img src="https://images.pexels.com/photos/3972753/pexels-photo-3972753.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" /> -->
-			<img src="https://images.pexels.com/photos/441179/pexels-photo-441179.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
+			<!-- <img src="https://images.pexels.com/photos/441179/pexels-photo-441179.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" /> -->
 			<!-- <img src="https://images.pexels.com/photos/1429775/pexels-photo-1429775.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"/> -->
 			<div class="info">
 				<div>
-					<h3>Looking For Your Perffect <em>Car</em>?</h3>
+					<h3>www.usedmotorcars.com</h3>
 					<h6 class="secondary-button">
 						<a href="list.php" style="padding: 16px 50px;">View All Cars <i class="fa fa-car"></i></a>
 					</h6>
