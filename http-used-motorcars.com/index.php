68efc8844b37432fbf83f57bde94d2c6ff0dc535
suren vithanage <surenanthonyvithanage@gmail.com>
2021-07-30 19:48:33 +0530

    banner modifications

--- http-used-motorcars.com/index.php
+++ http-used-motorcars.com/index.php
@@ -6,18 +6,37 @@
 	<div class="item">
 		<div class="img-fill">
 			<!-- <img src="assets/images/banner/banner_flat_10.jpg" alt="banner"> -->
+			<img src="assets/images/banner/banner_allion_2.jpg" alt="banner">
+			
 			<!-- <img src="https://images.pexels.com/photos/3972753/pexels-photo-3972753.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" /> -->
 			<!-- <img src="https://images.pexels.com/photos/441179/pexels-photo-441179.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" /> -->
 			<!-- <img src="https://images.pexels.com/photos/1429775/pexels-photo-1429775.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"/> -->
 			<div class="info">
 				<div>
-					<h4 style="    text-transform: uppercase;
-    font-weight: 800;">www.usedmotorcars.com</h4>
+					<h4 style="text-transform: uppercase;font-weight: 800;">www.usedmotorcars.com</h4>
 					<p style=" font-weight: 800;">sales@used-motorcars.com <br/>
 					Tel : +81-52-351-5541</p>
 					<h6 class="secondary-button">
 						<a href="list.php" style="padding: 16px 50px;">View All Cars <i class="fa fa-car"></i></a>
 					</h6>
+				</div>
+			</div>
+
+			<div class="infoLeftContent">
+				<div>
+					<p style=" font-weight: 800; font-size: 25px; color: rgb(102, 0, 0);">Car shopping from the <br/> comfort of your home!</p>
+				</div>
+			</div>
+
+			<div class="infoRightContent">
+				<div>
+					<p style=" font-weight: 800; font-size: 25px; color: rgb(102, 0, 0);">Click! Click!! <br/> The dealer at your fingertips!!!</p>
+				</div>
+			</div>
+
+			<div class="infoBottomContent">
+				<div>
+					<p style=" font-weight: 800; font-size: 25px; color: rgb(102, 0, 0);">It's that easy... Let's find your next car..</p>
 				</div>
 			</div>
 		</div>
